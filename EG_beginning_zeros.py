b645767c0e0a9772ecb1e2c6df08050d652fb539
emilkaa <milka.godlewska@gmail.com>
2021-06-21 21:40:40 +0200

    Add files via upload

---
+++ EG_beginning_zeros.py
@@ -0,0 +1,23 @@
+def beginning_zeros(number: str) -> int:
+    y = str(number)
+    end = 0
+    for i in y:
+        if i == "0":
+            end += 1
+        else:
+            break
+    return end
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(beginning_zeros('100'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert beginning_zeros('100') == 0
+    assert beginning_zeros('001') == 2
+    assert beginning_zeros('100100') == 0
+    assert beginning_zeros('001001') == 2
+    assert beginning_zeros('012345679') == 1
+    assert beginning_zeros('0000') == 4
+    print("Coding complete? Click 'Check' to earn cool rewards!")

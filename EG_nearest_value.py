efd322fc00599100fb82f5bbf856563b256f7e03
emilkaa <milka.godlewska@gmail.com>
2021-06-22 20:14:31 +0200

    Add files via upload

---
+++ EG_nearest_value.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+def nearest_value(values: set, one: int) -> int:
+    li = list(values)
+    dl =len(li)
+    end = li[-1]
+    print(end)
+    print(dl)
+    if one in li:
+        return one
+    if one > end:
+        return values[-1]
+    else:
+        li.sort()
+        # difference_array = np.absolute(li-one)
+        # absolute_difference_function = lambda list_value: abs(li - one)
+        # closest_value = min(li, key=absolute_difference_function)
+        y = min(li, key=lambda x: abs(x - one))
+        return y
+
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(nearest_value({4, 7, 10, 11, 12, 17}, 100))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
+    assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
+    assert nearest_value({4, 9, 10, 11, 12, 17}, 9) == 9
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 0) == 4
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
+    assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
+    assert nearest_value({-1, 2, 3}, 0) == -1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
